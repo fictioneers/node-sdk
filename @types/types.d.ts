3c4e4d5229d9de0332d1eab19e8891faf71121ce
Rob Caiger <rob@hyperact.co.uk>
2022-11-18 09:59:13 +0000

    V2 approach

---
+++ @types/types.d.ts
@@ -0,0 +1,16 @@
+import { components } from "./openapi";
+export interface AccessTokenResponse {
+    accessToken: string;
+    expiresIn: number;
+}
+export interface DeleteResponse {
+    data: null;
+    error: string | null;
+    meta: any;
+    status: number;
+}
+export interface InitialiseAndProgressUser {
+    userStoryState?: components["schemas"]["UserStoryState"] | null;
+    userTimelineEvents: components["schemas"]["UserTimelineEvent"][];
+}
+export type State = components["schemas"]["UserInvokedTimelineEventState"];
